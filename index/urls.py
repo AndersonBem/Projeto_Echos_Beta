77629f88760f1cf028a1bb8fbba9443bc90fbbda
AndersonBem <anderson.bem@hotmail.com>
2023-09-11 11:37:50 -0300

    11/09 commit

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from index.views import index
+from index.views import index,lista_veterinarios,lista_clinicas,lista_pacientes,lista_tutores,nova_clinica,novo_paciente,novo_tutor,novo_veterinario
 
 urlpatterns = [
-    path('',index),
+    path('',index, name='index'),
+    path('lista_veterinarios/', lista_veterinarios, name='lista_veterinarios'),
+    path('lista_clinicas/', lista_clinicas, name='lista_clinicas'),
+    path('lista_pacientes/',lista_pacientes, name='lista_pacientes'),
+    path('lista_tutores', lista_tutores, name='lista_tutores'),
+    path('nova_clinica', nova_clinica, name='nova_clinica'),
+    path('novo_paciente', novo_paciente, name='novo_paciente'),
+    path('novo_tutor', novo_tutor, name='novo_tutor'),
+    path('novo_veterinario', novo_veterinario, name='novo_veterinario'),
 ]
--- index/views.py
+++ index/views.py
@@ -6,3 +6,26 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index/index.html')
 
+def lista_veterinarios(request):
+    return render(request, 'index/lista_veterinarios.html')
+
+def lista_clinicas(request):
+    return render(request,'index/lista_clinicas.html')
+
+def lista_pacientes(request):
+    return render(request, 'index/lista_pacientes.html')
+
+def lista_tutores(request):
+    return render(request, 'index/lista_tutores.html')
+
+def nova_clinica(request):
+    return render(request,'index/nova_clinica.html')
+
+def novo_paciente(request):
+    return render(request,'index/novo_paciente.html')
+
+def novo_tutor(request):
+    return render(request,'index/novo_tutor.html')
+
+def novo_veterinario(request):
+    return render(request,'index/novo_veterinario.html')
