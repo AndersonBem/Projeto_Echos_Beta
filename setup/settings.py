6b55decea5ab94d709b17a1c167a858b0630b7f0
AndersonBem <anderson.bem@hotmail.com>
2023-10-23 11:20:08 -0300

    23/10 correção pass commit

--- setup/settings.py
+++ setup/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Projeto Echos',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': str(os.getenv('PASSWORD')),
         'HOST': 'localhost',
         'PORT': '5432',
     }
